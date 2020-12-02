f8e9cb86814dfc71279960af707927f7bc360517
Trịnh Xuân Đạt <datbmtt1@gmail.com>
2020-12-02 23:33:20 +0700

    update screen login & signup

--- note_app/lib/src/resources/login/signup_screen.dart
+++ note_app/lib/src/resources/login/signup_screen.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:note_app/src/components/already_have_an_account_acheck.dart';
 import 'package:note_app/src/components/background_login.dart';
+import 'package:note_app/src/components/or_divider.dart';
 import 'package:note_app/src/components/rounded_button.dart';
 import 'package:note_app/src/components/rounded_input_field.dart';
 import 'package:note_app/src/components/rounded_password_field.dart';
-import 'package:note_app/src/resources/login/signup_screen.dart';
+import 'package:note_app/src/components/social_icon.dart';
+import 'package:note_app/src/resources/login/login_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({
-    Key key,
-  }) : super(key: key);
-
+class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -21,15 +19,14 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "LOGIN",
+              "SIGNUP",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: size.height * 0.03),
             SvgPicture.asset(
-              "assets/icons/login1.svg",
+              "assets/icons/signup1.svg",
               height: size.height * 0.35,
             ),
-            SizedBox(height: size.height * 0.03),
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
@@ -38,22 +35,37 @@ class LoginScreen extends StatelessWidget {
               onChanged: (value) {},
             ),
             RoundedButton(
-              text: "LOGIN",
+              text: "SIGNUP",
               press: () {},
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
+              login: false,
               press: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return SignUpScreen();
+                      return LoginScreen();
                     },
                   ),
                 );
               },
             ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocalIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/google.svg",
+                  press: () {},
+                ),
+              ],
+            )
           ],
         ),
       ),
